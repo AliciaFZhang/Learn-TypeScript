84aacda15173f40aacce455d8e61814c7b23abf3
Fan <aliciafzhang@gmail.com>
2022-07-28 22:39:20 -0700

    learn typescript todolist

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,58 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-import confetti from 'canvas-confetti';
+import { v4 as uuidV4 } from 'uuid';
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+// console.log(uuidV4());
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+form?.addEventListener("submit", e=> {
+  e.preventDefault()
+  console.log("clicked")
+  if (input?.value == "" || input?.value == null) return
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  tasks.push(newTask)
+  addListItem(newTask)
+  input.value = " "
+
+})
+
+function addListItem(task: Task): boolean {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+  })
+  saveTasks()
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+  return true
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
